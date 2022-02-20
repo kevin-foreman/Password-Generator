f37ca972940037a713fb18f73f466fbe81532bfc
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-02-20 13:06:33 -0600

    add all prompt boxes and some if loops to prevent empty responses

--- Develop/script.js
+++ Develop/script.js
@@ -29,19 +29,66 @@ var getRandomSpecial = function() {
 
 var numbers = [1,2,3,4,5,6,7,8,9,0];
 var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
-var letters = Array.from(Array(26)).map( (_, i) => i + 97)
+var letters = Array.from(Array(26)).map( (_, i) => i + 97);
+var LowCaseLetters = letters.map(code => String.fromCharCode(code));
+var UpCaseLetters = LowCaseLetters.map(letter => letter.toUpperCase())
 
 // Generate password function
 var generatePassword = function(length, hasNumbers, hasLowercase, hasUppercase, hasSymbols) {
   // prompt user to specify password length
- var promptLength = window.prompt("Please specify a password length between 18 and 36 characters.");
+ var promptLength = window.prompt("Please specify a password length between 18 and 36 characters. This password generator will prioritize length over complexity, so if you select only one character parameter your password will not be the strongest it can be.");
+
+ // function isLetter(str) {
+   // return str.length === 1 && str.match(/[a-z]/i);
+ // }
 
  if (promptLength === "" || promptLength === null) {
   window.alert("You need to specify a length! Please try again.");
   return generatePassword();
-  
-}
  }
+
+ if (promptLength < 17 || promptLength > 35) {
+   window.alert("You need to specify a length within the range. Please try again.");
+   return generatePassword();
+ }
+
+ if (promptLength === ) {
+   window.alert("You need to specify a number between 18 and 36. Please try again");
+   return generatePassword();
+ }
+
+  // prompt user to specify upper case letters or not
+ var promptUpper = window.prompt("Do you want to include upper case letters? 1 for yes, 2 for no.");
+ 
+ if (promptUpper === "" || promptUpper === null) {
+  window.alert("You need to decide! Please try again.");
+  return generatePassword();
+ }
+
+  // prompt user to specify lower case letters or not
+  var promptLower = window.prompt("Do you want to include lower case letters? 1 for yes, 2 for no.");
+ 
+  if (promptLower === "" || promptLower === null) {
+   window.alert("You need to decide! Please try again.");
+   return generatePassword();
+  }
+
+  // prompt user to specify numbers or not
+  var promptNumbers = window.prompt("Do you want to include numbers? 1 for yes, 2 for no.");
+ 
+ if (promptNumbers === "" || promptNumbers === null) {
+  window.alert("You need to decide! Please try again.");
+  return generatePassword();
+ }
+  
+  // prompt user to specify special characters or not
+  var promptSpecial = window.prompt("Do you want to include special characters or not? 1 for yes, 2 for no.");
+ 
+ if (promptSpecial === "" || promptSpecial === null) {
+  window.alert("You need to decide! Please try again.");
+  return generatePassword();
+ }
+};
 
 // console.log(string.fromCharCode(97));
 
